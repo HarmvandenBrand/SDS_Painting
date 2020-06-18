8d0d715c15ca13d2a762f8cadcfa33f091095c31
Harm <harmpie112@hotmail.com>
2020-06-18 16:54:31 +0200

    Added black_rebels and a folder with the final input images.

--- Main2.py
+++ Main2.py
@@ -39,6 +39,16 @@ def is_active(agent, canvas, target_color, alpha):
             canvas.original_image[agent[0]][agent[1]][2] - target_color[2]) ** 2) < alpha
 
 
+def color_distance(color1, color2):
+    if not (len(color1) == len(color2) and len(color1) == 3):
+        raise TypeError(f"Either of {color1} or {color2} is not a valid color.")
+    return math.sqrt(sum([(color1[i] - color2[i])**2 for i in range(len(color1))]))
+
+
+def black_active(agent, canvas):
+    return sum(canvas.original_image[agent[0]][agent[1]]) == 0
+
+
 # The big function
 def SDS(agent_locs, num_agents, target_color, alpha, canvas, epochs, brush):
 
@@ -138,32 +148,46 @@ def SDS_rebels(agent_locs, rebel_locs, target_color, alpha, canvas, epochs, brus
     return canvas
 
 
+def SDS_black_swarm(agent_locs, black_locs, target_color, alpha, canvas, epochs, brush):
 
-def color_distance(color1, color2):
-    if not (len(color1) == len(color2) and len(color1) == 3):
-        raise TypeError(f"Either of {color1} or {color2} is not a valid color.")
-    return math.sqrt(sum([(color1[i] - color2[i])**2 for i in range(len(color1))]))
+    height, width = np.shape(canvas.get_image())[:2]
+
+    for e in range(epochs):
+        for black in black_locs:
+            # if not black[2]:
+            black[0] = random.randint(0, height-1)
+            black[1] = random.randint(0, width-1)
+            black[2] = black_active(black, canvas)
+
+
+    mixed_agents = list(agent_locs)
+    mixed_agents.extend(black_locs)
+
+    SDS(mixed_agents, len(mixed_agents), target_color, alpha, canvas, epochs, brush)
+
+    return canvas
 
 
 if __name__ == "__main__":
 
     # Load input image
-    input_img = imageio.imread('mao.jpg')
+    input_img = imageio.imread('Input_Images/MountainScape_Square_750.jpg')
     height, width = input_img.shape[0], input_img.shape[1]
 
     # any brush strokes with a size above this value are not guaranteed to paint. Adjust as you see fit
     MAX_BRUSH_SIZE = 400
 
     # paper uses w*h / 5
-    num_agents = int((width * height) / 500)
+    num_agents = int((width * height) / 50)
     num_rebels = int(num_agents/20)
+    num_black_swarm = int(num_agents/50)
     # maximum value of color distance that makes an agent happy
-    alpha = 20
-    brush_size = 12
+    alpha = 25
+    brush_size = 24
     # epochs per target color
-    epochs = 2
+    epochs = 5
     # number of colors to target and run SDS on
-    num_colors = 250
+    num_colors = 50
 
     canvas = paint.Canvas(input_img, max_brush_size=MAX_BRUSH_SIZE)
 
@@ -171,14 +195,15 @@ if __name__ == "__main__":
     """Extension toggles"""
     brushsize_annealing = True
     used_colors_alpha = 0 # The minimum color distance from all used colors for a new target color to be accepted. Set to <0 if repeats are okay.
-    david_bowie = False
+    david_bowie = True
+    black_swarm = False
 
     if brushsize_annealing:
         brush_size *= 2
 
     # Initialize brush
-    brush = paint.BrushRound(brush_size, [0, 0, 0], opacity=1)
-    # brush = paint.BrushSquare(brush_size, [0, 0, 0], opacity=1)
+    brush = paint.BrushRound(brush_size, [0, 0, 0], opacity=0.5)
+    # brush = paint.BrushSquare(br  ush_size, [0, 0, 0], opacity=1)
     # brush = paint.BrushSquare(brush_size, [0, 0, 0], opacity=0.2, opacity_falloff='cornered')
     # brush = paint.BrushSquare(brush_size, [0, 0, 0], opacity=1, opacity_falloff='linear')
 
@@ -187,6 +212,7 @@ if __name__ == "__main__":
 
     # Use a seed to be able to reproduce results
     seed = random.randint(0, 1_000_000_000)
+    # seed = 6916708
     print(f"Using seed {seed}.")
     random.seed(seed)
 
@@ -200,6 +226,10 @@ if __name__ == "__main__":
         if david_bowie:
             rebels = [[x, y, True] for _ in range(num_rebels) for x in random.choices(range(height)) for y in
                           random.choices(range(width))]
+
+        if black_swarm:
+            black_locs = agent_locs = [[x, y, False] for _ in range(num_black_swarm) for x in random.choices(range(height)) for y in
+                      random.choices(range(width))]
 
         """Target color selection"""
 
@@ -219,8 +249,11 @@ if __name__ == "__main__":
 
         if david_bowie:
             canvas = SDS_rebels(agent_locs, rebels, target_color, alpha, canvas, epochs, brush)
+        elif black_swarm:
+            canvas = SDS_black_swarm(agent_locs, black_locs, target_color, alpha, canvas, 4, brush)
         else:
             canvas = SDS(agent_locs, num_agents, target_color, alpha, canvas, epochs, brush)
+
 
         print(f"Painted color {i+1}: {target_color}.")
 
@@ -228,6 +261,6 @@ if __name__ == "__main__":
             brush.resize(int(brush_size * (1 - (i/num_colors))))
 
     display_image(canvas.get_image())
-    imageio.imwrite("result_mao1.png", canvas.get_image())
+    imageio.imwrite("result.png", canvas.get_image())
 
     make_gif(gif_images)
