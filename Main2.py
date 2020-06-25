50181bccccd6b3a4ef882c5d24d856b53897dd90
Harm <harmpie112@hotmail.com>
2020-06-25 16:03:51 +0200

    Code cleanup plus improved commentary

--- Main2.py
+++ Main2.py
@@ -1,11 +1,8 @@
 import numpy as np
-import PIL.Image as Image
 from matplotlib import pyplot as plt
 import imageio
 import random
 import math
-import os
-import shutil
 import copy
 
 import paint
@@ -17,14 +14,14 @@ def display_image(array):
     plt.show()
 
 
+# Function to make a gif file out of the images saved during each epoch.
 def make_gif(images):
-
     with imageio.get_writer('resultgif.gif', mode='I', duration=0.1) as writer:
         for image in images:
             writer.append_data(image)
 
 
-# Gaussian int with an upper and lower bound
+# Gaussian int with an upper and lower bound. Used when placing an inactive agent near another active agent.
 def trunc_gauss(mu, sigma, bottom, top):
     a = int(random.gauss(mu, sigma))
     while (bottom <= a <= top) == False:
@@ -40,6 +37,7 @@ def is_active(agent, canvas, target_color, alpha):
 
 
 # The big function
+# This function runs SDS for a specified number of epochs. For each call to this function, only one target color is used.
 def SDS(agent_locs, num_agents, target_color, alpha, canvas, epochs, brush):
 
     height, width = np.shape(canvas.get_image())[:2]
@@ -48,34 +46,41 @@ def SDS(agent_locs, num_agents, target_color, alpha, canvas, epochs, brush):
     initial_brush_size = brush.size
 
     for epoch in range(epochs):
+
+        # End if all agents are active
         if active_agents == num_agents:
             break
+
         print("epoch: ", epoch)
+
+        # Update activity of agents
         for agent in agent_locs:
             if is_active(agent, canvas, target_color, alpha):
                 agent[2] = True
                 active_agents += 1
 
+        # Resize brush according to ratio of active to inactive agents
         brush.resize(initial_brush_size * (1 - int(active_agents / num_agents)))
-        # brush_size = brush_size * (1 - int(active_agents / num_agents))
-        # brush.resize(brush_size)
 
-
+        # Each inactive agent finds a random other agent
         for i, agent in enumerate(agent_locs):
             if not agent[2]:
                 numbers = list(range(0, i)) + list(range(i + 1, len(agent_locs)))
                 r = random.choice(numbers)
+
+                # If the other agent is active, this first agent is placed near it
                 if agent_locs[r][2]:
                     active_agent_x = agent_locs[r][1]
                     active_agent_y = agent_locs[r][0]
                     agent[0] = trunc_gauss(active_agent_y, 5, 0, height - 1)
                     agent[1] = trunc_gauss(active_agent_x, 5, 0, width - 1)
 
-                    # Paint
+                    # Paint if this place is good enough
                     if is_active(agent, canvas, target_color, alpha):
                         brush.color = canvas.original_image[active_agent_y, active_agent_x]
                         brush.stroke(canvas, agent[0], agent[1])
 
+                # If the other agent is
                 else:
                     agent[0] = random.choice(range(height))
                     agent[1] = random.choice(range(width))
@@ -83,6 +88,7 @@ def SDS(agent_locs, num_agents, target_color, alpha, canvas, epochs, brush):
         #Add image for later creating a process gif
         gif_images.append(copy.deepcopy(np.copy(canvas.get_image())))
 
+    # Reset brush to initial size
     brush.resize(initial_brush_size)
 
     return canvas
@@ -97,12 +103,13 @@ def color_distance(color1, color2):
 if __name__ == "__main__":
 
     # Load input image
-    input_img = imageio.imread('mao.jpg')
+    input_img = imageio.imread('Input_Images/Frogger_750.jpg')
     height, width = input_img.shape[0], input_img.shape[1]
 
-    # any brush strokes with a size above this value are not guaranteed to paint. Adjust as you see fit
+    # Any brush strokes with a size above this value are not guaranteed to paint. Adjust as you see fit
     MAX_BRUSH_SIZE = 400
 
+    """Important parameters"""
     # paper uses w*h / 5
     num_agents = int((width * height) / 50)
     # maximum value of color distance that makes an agent happy
@@ -110,7 +117,7 @@ if __name__ == "__main__":
     brush_size = 5
     # epochs per target color
     epochs = 10
-    # number of colors to target and run SDS on
+    # number of different colors to target and run SDS on
     num_colors = 5
 
     canvas = paint.Canvas(input_img, max_brush_size=MAX_BRUSH_SIZE)
@@ -138,7 +145,7 @@ if __name__ == "__main__":
     print(f"Using seed {seed}.")
     random.seed(seed)
 
-
+    """This is where the actual algorithm starts"""
     for i in range(num_colors):
 
         # Initialize agents
@@ -169,6 +176,8 @@ if __name__ == "__main__":
             brush.resize(int(brush_size * (1 - (i/num_colors))))
 
     display_image(canvas.get_image())
-    imageio.imwrite("result_mao1.png", canvas.get_image())
+    imageio.imwrite("result.png", canvas.get_image())
 
     make_gif(gif_images)
+
+    print("\nDone!")
